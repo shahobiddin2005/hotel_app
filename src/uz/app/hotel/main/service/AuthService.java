d1dda574637b4f1bc10a9da74d517cb01bc532a9
shahobiddin2005 <uralovshahobiddin2005@gmail.com>
2024-05-18 16:07:20 +0500

    Boshladik

--- src/uz/app/hotel/main/service/AuthService.java
+++ src/uz/app/hotel/main/service/AuthService.java
@@ -3,7 +3,7 @@ package uz.app.hotel.main.service;
 public class AuthService implements uz.app.hotel.service.AuthService {
     @Override
     public void signUp() {
-
+        String
     }
 
     @Override
---
+++ src/uz/app/hotel/main/service/Utils.java
@@ -0,0 +1,27 @@
+package uz.app.hotel.main.service;
+
+import java.util.Scanner;
+
+public class Utils {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner strScanner = new Scanner(System.in);
+
+    public static Integer getInt(String message){
+        try {
+            System.out.println(message);
+            return scanner.nextInt();
+        }catch (Exception e){
+            scanner.nextLine();
+            return getInt(message);
+        }
+    }
+    public static Double getDouble(String message){
+        try {
+            System.out.println(message);
+            return scanner.nextDouble();
+        }catch (Exception e){
+            scanner.nextLine();
+            return getDouble(message);
+        }
+    }
+}
